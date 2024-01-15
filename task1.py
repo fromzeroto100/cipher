7755d5095ec24287d4e52533e6fe23a0cf04b66e
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-15 19:51:12 +0100

    new2

--- task1.py
+++ task1.py
@@ -1,7 +1,13 @@
-def greet():
-    print("Hello")
-    print("Hello")
-    print("Hello")
+# def greet():
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
 
-greet()    
+# greet()    
     
+def greet(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}? ")
+
+
+greet("Yasmina")    
